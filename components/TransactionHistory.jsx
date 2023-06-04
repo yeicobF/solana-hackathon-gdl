5e02e30a8b188f0df5e69cb35f860eb46279e9c5
yeicobF <jacobrdz34@gmail.com>
2023-06-04 16:21:56 -0600

    Fix: Add key to component

--- components/TransactionHistory.jsx
+++ components/TransactionHistory.jsx
@@ -1,36 +1,40 @@
 import Link from "next/link"
 import { WalletIcon } from "./icons/WalletIcon"
 
-
-export  function RecordOperationContainer({ record }) {
-    return (
-      <Link href={`/remittances/${record.transactionId}`} className="flex rounded-lg justify-start items-center  gap-8 hover:bg-slate-300 py-2 px-4">
-  g
-        <WalletIcon />
-        <div className="flex items-start flex-col">
-          <h3 className="first-letter:uppercase  font-bold text-lg">
-            {record?.name}
-          </h3>
-          <h4 className="first-letter:uppercase  font-light">
-            {record?.date}
-          </h4>
-        </div>
-        <div className="flex content-center flex-col">
-          <h4 className="first-letter:uppercase  font-normal">
-            {record?.type}
-          </h4>
-        </div>
-        <div className="flex content-center flex-col">
+export function RecordOperationContainer({ transaction }) {
+  return (
+    <Link
+      href={`/remittances/${transaction.transactionId}`}
+      className="flex rounded-lg justify-start items-center  gap-8 hover:bg-slate-300 py-2 px-4"
+    >
+      <WalletIcon />
+      <div className="flex items-start flex-col">
+        <h3 className="first-letter:uppercase  font-bold text-lg">
+          {transaction?.name}
+        </h3>
+        <h4 className="first-letter:uppercase  font-light">
+          {transaction?.date}
+        </h4>
+      </div>
+      <div className="flex content-center flex-col">
         <h4 className="first-letter:uppercase  font-normal">
-            ${record?.amount}
-          </h4>
-        </div>
-      </Link>
-    )
-  }
+          {transaction?.type}
+        </h4>
+      </div>
+      <div className="flex content-center flex-col">
+        <h4 className="first-letter:uppercase  font-normal">
+          ${transaction?.amount}
+        </h4>
+      </div>
+    </Link>
+  )
+}
 
-  export function TransactionHistory({transactions}){
-    return transactions.map(transaction => (
-        <RecordOperationContainer record={transaction}/>
-      ))
-  }
+export function TransactionHistory({ transactions }) {
+  return transactions.map((transaction) => (
+    <RecordOperationContainer
+      key={transaction.transactionId}
+      transaction={transaction}
+    />
+  ))
+}
